57212aa1bc6e226b75145c4ec51ed387b1c465d6
Brandon Lecky <dreamxinxcode@gmail.com>
2020-07-30 22:15:56 +0000

    serperate modules

--- play.js
+++ play.js
@@ -1,18 +1,5 @@
 const net = require('net');
-
-// Establish connection with the game server
-const connect = () => {
-  const conn = net.createConnection({
-    host: '192.168.1.64',
-    port: 50541
-  });
-  conn.setEncoding('utf8');
-  // attach an event handler to handle incoming data and console log it for the player.
-  conn.on('data', (data) => {
-    console.log(data);
-  });
-  return conn;
-};
+const connect = require('./client');
 
 console.log('Connecting..');
 connect();
